1b75c4e890e0170b9239edd5bfb9151240f68a43
hrafag <afagnd@code.edu.az>
2022-04-13 20:45:17 +0400

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Regemi daxil edin");
+            int num = Convert.ToInt32(Console.ReadLine());
+               
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Reqem cutdur");
+
+            }
+            else
+            {
+                Console.WriteLine("Reqem tekdir");
+            }
+        
+
+
+            {
+               
+
+            }
+                
+
+
+
+        }
+    }
+    
+
+        
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Birinci reqemi daxil edin");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci reqemi daxil edin");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 == num2) { Console.WriteLine("her iki rfeqem eynidir"); }
+            else if (num1 > num2) { Console.WriteLine(num1); }
+            else { Console.WriteLine(num2); }
+
+            
+          
+              
+            {
+
+            }
+        }
+    }
+}
